61745e866154dbbe7082e2e33c58882f1eb96feb
faezrezaee <84761467+faezrezaee@users.noreply.github.com>
2022-11-24 17:18:01 +0330

    Update App.py

--- App.py
+++ App.py
@@ -1,7 +1,5 @@
 from download import download
-format = ["zip","tar","file","tar.gz"]
 while True :
     url = input("please Enter donload link : ")
     link = download(url,"file",replace=True)
     print("============================\nyour file downloaded \n============================\n")
-    
